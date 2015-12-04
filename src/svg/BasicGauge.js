fc43c0fbde56eb543b819a034929ae1c147c9fd6
Dean Shub <deanshub@gmail.com>
2015-12-04 23:15:18 +0200

    fixing the gauge calculations

--- src/svg/BasicGauge.js
+++ src/svg/BasicGauge.js
@@ -16,27 +16,22 @@ export class BasicGauge extends Component {
 
   calcPath(radius){
     let angleFromVal = this.props.value * 3.6;
-    let cx=110,cy=100;
+    let cx=370,cy=100;
     let startAngle=0,
       endAngle=angleFromVal-90;
 
-    let x1=cx;
-    let y1=cy-radius;
-    let inside=((angleFromVal===360)||(angleFromVal%360>180))?"1":"0";
-    // let x2 = parseInt(110 + radius*Math.cos(Math.PI*startAngle/180));
-    // let y2 = parseInt(100 + radius*Math.sin(Math.PI*startAngle/180));
+    let inside=((angleFromVal===360)||(angleFromVal%360>180))?'1':'0';
 
-    let x2 = parseInt(110 + radius*Math.cos(Math.PI*endAngle/180));
-    let y2 = parseInt(100 + radius*Math.sin(Math.PI*endAngle/180));
+    let x2 = radius*Math.cos(Math.PI*endAngle/180);
+    let y2 = radius+(radius*Math.sin(Math.PI*endAngle/180));
 
-    return "M"+cx+","+cy+"  L" + x1 + "," + y1 + " A"+radius+","+radius+" 0 "+inside+",1 " + x2 + "," + y2 + " z"; //1 means clockwise
+    return 'M'+cx+','+cy+' l0,-' + radius + ' a'+radius+','+radius+' 0 '+inside+',1 ' + x2 + ',' + y2 + ' L' + cx + ',' + cy + ' l0,-'+radius; //1 means clockwise
   }
 
   render(){
     return (
       <svg width="100%" height="70%">
-        <circle cx="110" cy="100" r="83" fill="white" />
-        <circle cx="110" cy="100" r="80" fill={this.props.color} opacity="0.3" />
+        <circle cx="50%" cy="100" r="80" fill={this.props.color} opacity="0.3" />
         <path d={this.calcPath(80)} fill={this.props.color}/>
       </svg>
     );
